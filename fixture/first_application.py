e0f98177078dfbfcd449fb946708fa2957d60ba4
Pavel Sazonov <pavelsazonov2015@gmail.com>
2019-08-12 19:32:31 +0300

    Begin_of_import

--- fixture/first_application.py
+++ fixture/first_application.py
@@ -1292,3 +1292,137 @@ class FirstApplicationHelper:
         wd.find_element_by_xpath("//button[@id='SignDataButton']").click()
 
         time.sleep(5)
+
+##################################################################################################################################################################################  Import
+    def create_import_first_application(self, pib, email, phone_number, fax_number, city, flat, street, index,
+                                 number_national_account, requisits_number_national_account, number_foreign_account,
+                                 requisits_number_forein_account, duns_number):
+        wd = self.app.wd
+        # У меню з ліва натискаємо пункт «Подання заяв»
+        wd.find_element_by_xpath("//ul[@id='aside-menu']/li[2]/div/a/div/span").click()
+
+        #        time.sleep(3)
+        # Натискаємо кнопку створити заяву (У правому верхньому куті Синій кружочок з білим хрестиком у центрі)
+        wd.find_element_by_xpath("//a[contains(@ href,'/App/AppTypes')]").click()
+
+        # На сторінці Створення нової заяви, на переліку заяв натискаємо «Заяви щодо провадження діяльності з імпорту лікарських засобів (крім активних фармацевтичних інгредієнтів)»
+        wd.find_element_by_xpath("//div[@id='content']/div[2]/div/div[2]").click()
+
+        # У вище вказанному переліку заяв обираємо пункт «Заява про отримання ліцензії на провадження діяльності з виробництва лікарських засобів (промислового)» (додаток 2)
+        wd.find_element_by_xpath("//*[contains(text(), 'Заява про отримання ліцензії на провадження діяльності з імпорту лікарських засобів')]").click()
+
+        # Заповнюємо форму: Організаційно-правова форма* «460 Громадська організація»
+        wd.find_element_by_xpath("//div[@id='content']/div[2]/div/form/div[2]/div/div/div/div").click()
+        wd.find_element_by_xpath("//div[@id='content']/div[2]/div/form/div[2]/div/div/div/ul/li[2]/span").click()
+
+        # Заповнюємо форму: Форма власності «40 Власність інших держав»
+        wd.find_element_by_xpath("//div[@id='content']/div[2]/div/form/div[2]/div[2]/div/div/div").click()
+        wd.find_element_by_xpath("//div[@id='content']/div[2]/div/form/div[2]/div[2]/div/div/ul/li[2]/span").click()
+
+        # Заповнюємо форму: Прізвищеб ім'я по батькові керівника юридичної особи  «Ступка Богдан Сильвестрович»
+        wd.find_element_by_xpath("//input[@id='OrgDirector']").send_keys(pib)
+
+        # Заповнюємо форму: E-mail* «test@test.ua»
+        wd.find_element_by_xpath("//input[@id='EMail']").send_keys(email)
+
+        # Заповнюємо форму: Номер телефону* «+380123456789»
+        wd.find_element_by_xpath("//input[@id='PhoneNumber']").send_keys(phone_number)
+
+        # Заповнюємо форму: Номер факсу «+380123456789»
+        wd.find_element_by_xpath("//input[@id='FaxNumber']").send_keys(fax_number)
+
+        # Заповнюємо форму: Населений пункт* «Дніпропетровська область, Тернівка»
+        wd.find_element_by_xpath("//input[@id='CityName']").send_keys(city)
+        wd.find_element_by_xpath("//div[contains(.,'Дніпро')]").click()
+
+        # Заповнюємо форму: Номер будинку, корпус або будівля, номер квартири або офісу* «13»
+        wd.find_element_by_xpath("//input[@id='Building']").send_keys(flat)
+
+        wd.find_element_by_xpath("//input[@id='StreetName']").send_keys("Тестова")
+        time.sleep(2)
+        wd.find_element_by_xpath("//div[contains(.,'вул. Тестова Вулиця')]").click()
+
+        # Заповнюємо форму: Поштовий індекс* «12345»
+        wd.find_element_by_xpath("//input[@id='PostIndex']").send_keys(index)
+
+        # Заповнюємо форму: Номер рахунку в національній валюті «12345»
+        wd.find_element_by_xpath("//input[@id='NationalAccount']").send_keys(number_national_account)
+
+        # Заповнюємо форму: Реквизити банку з рахунком в національній валюті «23456»
+        wd.find_element_by_xpath("//input[@id='NationalBankRequisites']").send_keys(
+            requisits_number_national_account)
+
+        # Заповнюємо форму: Номер рахунку в іноземній валюті «34567»
+        wd.find_element_by_xpath("//input[@id='InternationalAccount']").send_keys(number_foreign_account)
+
+        # Заповнюємо форму: Реквизити банку з рахунком в іноземній валюті «45678»
+        wd.find_element_by_xpath("//input[@id='InternationalBankRequisites']").send_keys(
+            requisits_number_forein_account)
+
+        # Заповнюємо форму: D-U-N-S номер (за наявності) «56789»
+        wd.find_element_by_xpath("//input[@id='Duns']").send_keys(duns_number)
+        time.sleep(3)
+        # Натискаємо кнопку «ЗБЕРЕГТИ»
+        wd.find_element_by_xpath("//input[@value='Зберегти']").click()
+
+        # Перевірка переходу на сторінку "Заява про отримання ліцензії на провадження діяльності"
+        wd.find_element_by_xpath("//h1[contains(.,'Заява про отримання ліцензії на провадження діяльності')]")
+
+    def create_mpd_import(self, company_name, phone_number, email, fax_number, city, index, street, adress, building):
+        wd = self.app.wd
+        # Натискаємо кнопку «Місця проваждження діяльності»
+        wd.find_element_by_xpath("//div[@id='content-switcher']/a[2]/i").click()
+
+        # Натискаємо кнопку «Додати МПД» (з верху з права білий хрестик)
+        wd.find_element_by_xpath("//div[@id='content-btn']/a[2]").click()
+
+        # Заповнюємо форми Створення провадження діяльності: Найменування структурного підрозділу (або найменування юридичної особи):* «Тест»
+        wd.find_element_by_xpath("//input[@id='Name']").send_keys(company_name)
+
+        # Заповнюємо форми Створення провадження діяльності: Номер телефону* «+380123456789»
+        wd.find_element_by_xpath("//input[@id='PhoneNumber']").send_keys(phone_number)
+
+        # Заповнюємо форми Створення провадження діяльності: E-mail* «test@test.ua»
+        wd.find_element_by_xpath("//input[@id='EMail']").send_keys(email)
+
+        # Заповнюємо форми Створення провадження діяльності: Номер факсу* «+380123456789»
+        wd.find_element_by_xpath("//input[@id='FaxNumber']").send_keys(fax_number)
+
+        # Заповнюємо форми Створення провадження діяльності: Населений пункт* «Дніпропетровська область, Дніпро»
+        wd.find_element_by_xpath("//input[@id='CityName']").send_keys(city)
+        wd.find_element_by_xpath("//div[contains(.,'Дніпро')]").click()
+
+        # Заповнюємо форми Створення провадження діяльності: Поштовий індекс* «12345»
+        wd.find_element_by_xpath("//input[@id='PostIndex']").send_keys(index)
+
+        wd.find_element_by_xpath("//input[@id='StreetName']").send_keys("Тестова")
+        time.sleep(2)
+        wd.find_element_by_xpath("//div[contains(.,'вул. Тестова Вулиця')]").click()
+
+        # Заповнюємо форми Створення провадження діяльності: Адреса місця провадження діяльності (англійською)* «Test»
+        wd.find_element_by_xpath("//input[@id='AdressEng']").send_keys(adress)
+
+        # Заповнюємо форми Створення провадження діяльності: Номер будинку, корпус або будівля, номер квартири або офісу* «13»
+        wd.find_element_by_xpath("//input[@id='Building']").send_keys(building)
+
+        time.sleep(2)
+
+
+
+
+
+
+        # Обераємо чекбокс «Складські зони (приміщення для зберігання)»
+        wd.find_element_by_xpath("//label[contains(text(), 'Складські зони (приміщення для зберігання)')]").click()
+        wd.find_element_by_xpath("//label[contains(text(), 'Умови щодо контролю якості')]").click()
+        wd.find_element_by_xpath("//label[contains(text(), 'Імпорт зареєстрованих лікарських засобів у формі “in bulk” (продукції “in bulk”)')]").click()
+        wd.find_element_by_xpath("//label[contains(text(), 'Зони здійснення видачі дозволу на випуск (реалізацію) серії лікарського засобу')]").click()
+        wd.find_element_by_xpath("//label[contains(text(), 'Імпорт зареєстрованих готових лікарських засобів')]").click()
+
+
+
+        # Натискаємо кнопку «ЗБЕРЕГТИ»
+        wd.find_element_by_xpath("//input[@id='submitBranch']").click()
+
+        # Перевірка переходу на сторінку "Заява про отримання ліцензії на провадження діяльності"
+        wd.find_element_by_xpath("//h1[contains(.,'Заява про отримання ліцензії на провадження діяльності')]")
